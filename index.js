d31a92fdc144561e3ad937f68710d34deb577698
dannaemartinez <dannae.martinez@enroutesystems.com>
2022-04-06 08:19:12 +0000

    fibo

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
-function fibo() {
+function facto() {
+    const factoNumber = parseInt(document.getElementById("inputFacto").value);
+
+    if (factoNumber==0){
+      factoNumber.push(1);
+    } else{
+      factoNumber.push(factorial(factoNumber));
+      }
+
+    const container = document.getElementById("factoContainer");
+    const praragraph = document.createElement("p");
+    praragraph.innerText = (factoNumber);
+    container.appendChild(praragraph);
+  }
+  function factorial(number){
+    return number>1? number*factorial (number-1): 1;
+  }
+
+  /*function fibo() {
     const fiboNumber = parseInt(document.getElementById("inputFibo").value);
   
     console.log(fiboNumber);
@@ -16,24 +34,5 @@ function fibo() {
     const praragraph = document.createElement("p");
     praragraph.innerText = numberToShow.join(",");
     container.appendChild(praragraph);
-  }
-
-  function facto() {
-    const factoNumber = parseInt(document.getElementById("inputFacto").value);
-  
-    console.log(factoNumber);
-
-    if (factoNumber==0){
-      factoNumber.push(1);
-    } else{
-      factoNumber.push(factorial(factoNumber));
-      }
-
-    const cont = document.getElementById("factoContainer");
-    const pra = document.createElement("p");
-    cont.appendChild(pra);
-  }
-  function factorial(number){
-    return number>1? number*factorial (number-1): 1;
-  }
+  }*/
   
